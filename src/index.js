eacabe7c24c7a45b3356f0eb1d1cb01c1ef8631e
Svaigas <adamiecmateusz@wp.pl>
2020-02-05 13:53:48 +0100

    another set of changes 20200204

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import routesV1 from './routes/api/v1/routes'
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+app.use('/api/v1', routesV1)
 
 app.listen(PORT, async () => {
     console.log(`Server is listening on port: ${PORT}`)
---
+++ src/routes/api/v1/movies.js
@@ -0,0 +1,7 @@
+export function postMovieData(req, res) {
+    res.send(200)
+}
+
+export function getMoviesList(req, res) {
+    res.send(200)
+}
